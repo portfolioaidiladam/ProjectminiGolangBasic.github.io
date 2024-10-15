4392fa15bd36d3b3ad834d84ef4c184e8f48be3c
portfolioaidiladam <portfolioaidiladam@gmail.com>
2024-10-15 21:50:46 +0700

    first commit

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Aidil")
+	fmt.Println("Aidil Adam")
+	fmt.Println("Aidil Adam Baik")
+
+	fmt.Println(len("Aidil"))
+	fmt.Println("Aidil Adam"[0])
+	fmt.Println("Aidil Adam Baik"[1])
+}
---
+++ variable.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Aidil Adam"
+	fmt.Println(name)
+
+	name = "Aidil Baik"
+	fmt.Println(name)
+
+	var (
+		firstName  = "Aidil"
+		middleName = "Adam"
+		lastName   = "Baik"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(middleName)
+	fmt.Println(lastName)
+}
